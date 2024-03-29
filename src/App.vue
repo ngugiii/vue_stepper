c20154398b7449eb5a2dfc947a512a2cc232b02e
ngugiii <erickngugi54@gmail.com>
2024-03-29 09:32:38 +0300

    removed build files

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,57 @@
 <template>
   <div class="flex p-8 w-full flex-col items-center">
-    <div
-      class="shadow-md w-full md:w-[70%] rounded-lg flex justify-between p-4"
-    >
-      <template v-for="(step, index) in steps" :key="`step-${index}`">
-        <div
-          class="flex flex-col items-center"
-          :style="{
-            marginTop: index === 0 ? '0' : '8px',
-            marginBottom: index === steps.length - 1 ? '0' : '8px',
-          }"
-        >
-          <button
-            @click="setCurrentStep(index)"
-            class="focus:outline-none relative"
+    <div class="shadow-md w-full md:w-[70%] rounded-lg">
+      <div class="flex justify-between p-4 relative">
+        <template v-for="(step, index) in steps" :key="`step-${index}`">
+          <div
+            class="flex flex-col items-center"
+            :style="{
+              marginTop: '8px',
+              marginBottom: '8px',
+            }"
           >
-            <div class="w-10 h-10 rounded-full bg-white cursor-pointer">
-              <div
-                v-if="index < currentStep"
-                class="bg-green-500 rounded-full w-10 h-10 flex justify-center items-center z-10 relative"
-              >
-                <i class="fas fa-check text-white"></i>
+            <button @click="setCurrentStep(index)" class="focus:outline-none">
+              <div class="w-10 h-10 rounded-full bg-white cursor-pointer">
+                <div
+                  v-if="index < currentStep"
+                  class="bg-green-500 rounded-full w-10 h-10 flex justify-center items-center z-10 relative"
+                >
+                  <i class="fas fa-check text-white"></i>
+                </div>
+                <div
+                  v-else-if="index === currentStep"
+                  class="bg-blue-500 rounded-full w-10 h-10 flex justify-center items-center z-10 relative"
+                >
+                  <i class="fas fa-spinner fa-spin text-white"></i>
+                </div>
+                <div
+                  v-else
+                  class="bg-gray-300 w-full rounded-full h-10 flex justify-center items-center z-10 relative"
+                >
+                  <!-- <span class="text-gray-500">{{ index + 1 }}</span> -->
+                </div>
               </div>
-              <div
-                v-else-if="index === currentStep"
-                class="bg-blue-500 rounded-full w-10 h-10 flex justify-center items-center z-10 relative"
-              >
-                <i class="fas fa-spinner fa-spin text-white"></i>
+            </button>
+            <div class="top-[4rem]">
+              <div class="md:text-lg text-center md:w-[9rem] w-full">
+                {{ step.label }}
               </div>
-              <div
-                v-else
-                class="bg-gray-300 w-full rounded-full h-10 flex justify-center items-center z-10 relative"
-              >
-                <!-- <span class="text-gray-500">{{ index + 1 }}</span> -->
+              <div class="text-base text-gray-500 text-center w-full">
+                {{ step.description }}
               </div>
             </div>
-          </button>
-          <div class="md:text-lg text-center md:w-[9rem] w-full">
-            {{ step.label }}
           </div>
-          <div class="text-base text-gray-500 text-center w-full">
-            {{ step.description }}
-          </div>
-        </div>
-        <div
-          :class="{
-            'bg-green-500': index < currentStep,
-            'bg-blue-500': index === currentStep || index === currentStep - 1,
-            'bg-gray-300': index !== currentStep && index !== currentStep - 1,
-          }"
-          v-if="index !== steps.length - 1"
-          class="h-0.5 w-full mx-1 mt-[1.5rem]"
-        ></div>
-      </template>
+          <div
+            :class="{
+              'bg-green-500': index < currentStep,
+              'bg-blue-500': index === currentStep || index === currentStep - 1,
+              'bg-gray-300': index !== currentStep && index !== currentStep - 1,
+            }"
+            v-if="index !== steps.length - 1"
+            class="h-1 rounded-lg w-full mt-[1.5rem]"
+          ></div>
+        </template>
+      </div>
     </div>
     <div class="shadow-md rounded md:p-8 p-2 mt-6 w-full md:w-[70%]">
       <div
